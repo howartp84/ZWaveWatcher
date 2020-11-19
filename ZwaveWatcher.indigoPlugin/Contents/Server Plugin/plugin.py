39a5fa65746fd802d56c37771451630e0d09f56d
howartp84 <PHowarth@BlueYonder.co.uk>
2020-11-19 23:14:54 +0000

    v1.0.9

--- ZwaveWatcher.indigoPlugin/Contents/Server Plugin/plugin.py
+++ ZwaveWatcher.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -13,6 +13,8 @@ import sys
 
 import fnmatch
 
+import csv
+
 # Note the "indigo" module is automatically imported and made available inside
 # our global name space by the host process.
 
@@ -41,11 +43,48 @@ class Plugin(indigo.PluginBase):
 		self.nodeFromZed = dict()
 		self.nodeFromDev = dict()
 
+		self.csvCmds = dict()
+		self.csvTables = dict()
+		self.csvBits = dict()
+
+		reader = csv.DictReader(open('csvCmds.csv'))
+
+		result = {}
+		for row in reader:
+			key = row.pop('key')
+			result[key] = row
+		#self.debugLog(result)
+
+		self.csvCmds = result
+
+		reader = csv.DictReader(open('csvTables.csv'))
+
+		result = {}
+		for row in reader:
+			key = row.pop('key')
+			result[key] = row
+		#self.debugLog(result)
+
+		self.csvTables = result
+
+		reader = csv.DictReader(open('csvBits.csv'))
+
+		result = {}
+		for row in reader:
+			key = row.pop('key')
+			result[key] = row
+		#self.debugLog(result)
+
+		self.csvBits = result
+
+
+
 	########################################
 	def startup(self):
 		self.debugLog(u"startup called")
 		indigo.zwave.subscribeToIncoming()
 		indigo.zwave.subscribeToOutgoing()
+
 
 	def shutdown(self):
 		self.debugLog(u"shutdown called")
@@ -93,14 +132,21 @@ class Plugin(indigo.PluginBase):
 		bytes = byteListStr.split()
 		#nodeId = int(bytes[5],16)
 
+		#self.debugLog(int(bytes[5],16))
+
 		if (int(bytes[5],16)) in self.watchIDs:
+			devID = self.zedFromNode[int(bytes[5],16)]
+			classStr = indigo.devices[devID].ownerProps['zwClassCmdMapStr']
+			#self.debugLog(indigo.devices[devID])
+
 			if (endpoint == None):
 				self.debugLog(u"Raw command received (Node %s): %s" % ((int(bytes[5],16)),(byteListStr)))
+				self.cmdDecode(cmd,classStr)
 			else:
 				self.debugLog(u"Raw command received (Node %s Endpoint %s): %s" % ((int(bytes[5],16)),endpoint,(byteListStr)))
 			#self.debugLog(u"Node ID %s (Hex %s) found in watchIDs" % ((int(bytes[5],16)),(int(bytes[5],16))))
 		elif (int(bytes[5],16)) == 2:
-			if (bytes[7] == "86") and (bytes[8] == "11"): #Version 
+			if (bytes[7] == "86") and (bytes[8] == "11"): #Version
 				self.debugLog(u"Received 86 11 request.  Sending 86 12 02 04 020 07 01")
 				codeStr = [0x86, 0x12, 0x02, 0x04, 0x20, 0x07, 0x01] #Controller, 4.32, 7.1
 				indigo.zwave.sendRaw(node=2,cmdBytes=codeStr,sendMode=0,waitUntilAck=False)
@@ -108,7 +154,63 @@ class Plugin(indigo.PluginBase):
 				self.debugLog(u"Received 72 04 request.  Sending 72 05 01 4F 00 01 00 01")
 				codeStr = [0x72, 0x05, 0x01, 0x4F, 0x00, 0x01, 0x00, 0x01]
 				indigo.zwave.sendRaw(node=2,cmdBytes=codeStr,sendMode=0,waitUntilAck=False)
-			
+
+
+
+	def cmdDecode(self, cmd, classStr):
+		byteList = cmd['bytes']			# List of the raw bytes just received.
+		byteListStr = convertListToHexStr(byteList)
+		bytes = byteListStr.split()
+		#self.debugLog(classStr)
+
+		cmdClass = bytes[7]
+		cmdType = bytes[8]
+		cmdPos = classStr.index(cmdClass)
+		cmdVer = classStr[cmdPos+3:cmdPos+4] #25v1 => 1
+		cmdKey = "%s %s v%s" % (cmdClass,cmdType,cmdVer)
+
+		try:
+			cmdDict = self.csvCmds[cmdKey]
+		except KeyError as k:
+			self.debugLog("Command class: %sv%s" % (cmdClass,cmdVer))
+			self.debugLog("Command not yet decoded: %s" % cmdKey)
+			return
+
+		cmdName = cmdDict['cmdName']
+
+		self.debugLog("Command class: %sv%s (%s)" % (cmdClass,cmdVer,cmdName))
+
+		#self.debugLog(cmdDict)
+
+		byteSize = cmdDict['byteSize']
+		for i in range(9,int(byteSize)+9):
+			val = cmdDict[str(i)]
+			if "[TAB" in val:
+				tblPos = val.index("[TAB")
+				tblStr = "[TAB%s]" % val[tblPos+4:tblPos+6]
+				tblVal = self.csvTables[tblStr]
+				#self.debugLog("tblVal => %s" % tblVal)
+				if tblVal['fixed'] == "T":
+					for b in range(1,int(tblVal['size'])+1):
+						#self.debugLog("%s in %s" % (bytes[i],tblVal[str(b)]))
+						#self.debugLog(str(bytes[i] in tblVal[str(b)]))
+						if bytes[i] in tblVal[str(b)]:
+							self.debugLog(val.replace(tblStr,tblVal[str(b)]))
+				else:
+					self.debugLog(val.replace(tblStr,"0x%s, see table:" % bytes[i]))
+					for b in range(1,int(tblVal['size'])+1):
+						self.debugLog("   %s" % tblVal[str(b)])
+			elif "#" in val: #Decimal
+				self.debugLog("{} {}".format(val.replace(" #",""),int(bytes[i],16)))
+			elif "%" in val: #Percent
+				self.debugLog("{} {}%".format(val.replace(" %",""),int(bytes[i],16)))
+			elif "@" in val: #Both
+				self.debugLog("{} 0x{} ({})".format(val.replace(" @",""),bytes[i],int(bytes[i],16)))
+			else: #Hex by default
+				self.debugLog("{} 0x{}".format(val,bytes[i]))
+
+
+
 
 ########################################
 	def zwaveCommandSent(self, cmd):
